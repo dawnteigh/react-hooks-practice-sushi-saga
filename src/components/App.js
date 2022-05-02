ab3f672581bf50ca22279c41d9e24af0ad1542ea
Donte Ladatto <donte279@gmail.com>
2022-05-02 13:06:26 -0500

    complete with sushi wallet

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushi, setSushi] = useState([])
+  const [eaten, setEaten] = useState([])
+  const [position, setPosition] = useState(0)
+  const [money, setMoney] = useState(200)
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(data => setSushi(data))
+  }, [])
+
+  const sushiDisplay = sushi.slice(position, position + 4)
+
+  const handleEatSushi = (id, price) => {
+    if (!eaten.includes(id) && money >= price) {
+      const newEaten = [...eaten, id]
+      const newMoney = money - price
+      setEaten(newEaten)
+      setMoney(newMoney)
+    }
+  }
+
+  const handleMoreSushi = () => {
+    if (position === sushi.length - 4) {
+      setPosition(0)
+    } else {
+    setPosition(position + 4)
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushi={sushiDisplay} handleEatSushi={handleEatSushi} handleMoreSushi={handleMoreSushi} money={money} />
+      <Table setMoney={setMoney} money={money} plates={eaten} />
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Table({ plates = [] }) {
+function Table({ plates = [], money, setMoney }) {
+
+  const [cash, setCash] = useState(0)
+
   // renders an empty plate for every element in the array
   const emptyPlates = plates.map((_, index) => (
     <div key={index} className="empty-plate" style={{ top: -7 * index }} />
   ));
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setMoney(money + cash)
+    e.target.reset()
+  }
+
   return (
     <>
       <h1 className="remaining">
-        You have: ${/* Give me how much money I have left */} remaining!
+        You have: ${money} remaining!
       </h1>
       <div className="table">
         <div className="stack">{emptyPlates}</div>
+        <form onSubmit={handleSubmit} >
+        <input onChange={(e) => setCash(parseFloat(e.target.value))} type="number" step="0.01" placeholder="0.00" />
+        <input type="submit" value="Add Money"/>
+      </form>
       </div>
+      
     </>
   );
 }
