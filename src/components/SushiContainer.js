77ecf409ed3f769570125c8b6c9fb88065192af1
Donte Ladatto <donte279@gmail.com>
2022-05-02 13:05:02 -0500

    commit tree 6b00d4bc87268a3996d3ba3790f31405baa33575

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from './Sushi'
 
-function SushiContainer(props) {
+function SushiContainer({ sushi, handleEatSushi, handleMoreSushi, money }) {
+
+ 
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushi.map(s => {
+        return (<Sushi money={money} handleEatSushi={handleEatSushi} key={s.id} {...s} />)
+      })}
+      <MoreButton handleMoreSushi={handleMoreSushi} />
     </div>
   );
 }
